fb7fead13de07bcf8fbe089e7f613c77e839a0c8
CaptainDra <781706860@qq.com>
2020-05-21 23:18:14 -0400

    confirm page finished

--- WhatoEatDj/WhatoEatDj/urls.py
+++ WhatoEatDj/WhatoEatDj/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('inventory/', views.inventory ,name="inventory"),
+    path('menu/', views.menu ,name="menu"),
     path('login/',
          LoginView.as_view
          (
--- WhatoEatDj/app/views.py
+++ WhatoEatDj/app/views.py
@@ -6,6 +6,13 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 
+invResult = [
+    'egg','milk','rice','tomato'
+]
+inptDicLst = [
+    # 存放一些原始数据
+]
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
@@ -46,20 +53,6 @@ def about(request):
 
 def inventory(request):
     assert isinstance(request, HttpRequest)
-    if request.POST:
-        print('get post')
-        check_box_list = request.POST.getlist('check_box_list')
-        #text = request.POST.getlist('text',None)
-        print(check_box_list)
-        return render(
-        request,
-        'app/menu.html',
-        {
-            'title':'Inventory',
-            'message':'Choose what you have:',
-            'year':datetime.now().year,
-        }
-    ) 
     return render(
         request,
         'app/inventory.html',
@@ -70,5 +63,43 @@ def inventory(request):
         }
     )
     
+def menu(request):
+    assert isinstance(request, HttpRequest)
+    if request.POST:
+        print('get post from menu')
+        check_box_list = request.POST.getlist('check_box_list')
+        j = 0
+        result = []
+        inv =[]
+        #print(len(check_box_list))
+        for i in range(len(invResult)):
+            if invResult[i] == check_box_list[j]:
+                #print(invResult[i],'1')
+                tempDic = {'inv':invResult[i],'res':'1'}
+                inptDicLst.append(tempDic)
+                #inv.append(invResult[i])
+                #result.append('1')
+                if j+1 < len(check_box_list):
+                    j += 1
+            else:
+                tempDic = {'inv':invResult[i],'res':'0'}
+                inptDicLst.append(tempDic)
+                #print(invResult[i],'0')
+                #inv.append(invResult[i])
+                #result.append('0')
+        #tempDic = {'inv':inv,'res':result}
+        #inptDicLst.append(tempDic)
+        #text = request.POST.getlist('text',None)
+        #print(inptDicLst)
+        return render(
+        request,
+        'app/menu.html',
+        {
+            'title':'menu',
+            'message':'Confirm what you have:',
+            'year':datetime.now().year,
+            'list': inptDicLst,
+        }
+    ) 
 
 
